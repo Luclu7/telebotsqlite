10a8ab552bc1d6782aa55b4c500a002f6439c632
Luclu7 <me@luclu7.fr>
2019-08-26 04:08:17 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/luclu7/telebotsqlite
+
+go 1.12
+
+require (
+	github.com/mattn/go-sqlite3 v1.11.0
+	gopkg.in/tucnak/telebot.v2 v2.0.0-20190415090633-8c1c512262f2
+)
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	tb "gopkg.in/tucnak/telebot.v2"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var arrayOfItems []Item
+
+type Item struct {
+	Name     string
+	url      string
+	ThumbURL string
+}
+
+func logCommand(m *tb.Message, command string) {
+	t := time.Now()
+	log.Print(m.Sender.Username + " sent " + command + " at " + t.Format("2006-01-02 15:04:05"))
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select name,url,thumburl from stuff")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+		var url string
+		var thumburl string
+		err = rows.Scan(&name, &url, &thumburl)
+		if err != nil {
+			log.Fatal(err)
+		}
+		arrayOfItems = append(arrayOfItems, Item{Name: name, url: url, ThumbURL: thumburl})
+
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	token := os.Getenv("TELEBOT_TOKEN")
+	b, err := tb.NewBot(tb.Settings{
+		Token:  token,
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle(tb.OnQuery, func(q *tb.Query) {
+		results := make(tb.Results, len(arrayOfItems)) // []tb.Result
+		for i, item := range arrayOfItems {
+			if strings.Contains(item.Name, "photo") {
+				result := &tb.PhotoResult{
+					URL:      item.url,
+					ThumbURL: item.ThumbURL,
+				}
+				results[i] = result
+				results[i].SetResultID(strconv.Itoa(i)) // It's needed to set a unique string ID for each result
+			} else {
+				result := &tb.AudioResult{
+					Title: item.Name,
+					URL:   item.url,
+				}
+				results[i] = result
+				results[i].SetResultID(strconv.Itoa(i)) // It's needed to set a unique string ID for each result
+			}
+		}
+
+		err := b.Answer(q, &tb.QueryResponse{
+			Results:   results,
+			CacheTime: 60, // a minute
+		})
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	log.Print("The bot is starting...")
+	b.Start()
+}
